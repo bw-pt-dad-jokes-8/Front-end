832f1332144d6ec1ed9611096d811daa26266a5f
Michael Padiernos <mike.padiernos@gmail.com>
2019-12-21 16:37:42 -0500

    ADD: - created database of sample jokes - coded Jokes.js to pull from database

--- dadjokes/src/Components/Branding/Branding.js
+++ dadjokes/src/Components/Branding/Branding.js
@@ -6,6 +6,8 @@ import { Search } from 'grommet-icons';
 import BrandingBar from "./BrandingBar";
 import ShowSearch from "../ShowSearch/ShowSearch"
 
+import Logo from "../../logo.svg";
+
 export default function Branding(props) {
 
 	const [search, setSearch] = useState(false);
@@ -13,7 +15,10 @@ export default function Branding(props) {
 	return (
 		<>
 			<BrandingBar className="appbar">
-				<Heading level='1' color='brand' size='small' margin='none'><Link to="/">Dad, Don't Farce!</Link></Heading>
+				<Heading level='1' color='brand' size='small' margin='none'>
+					{/*<img className="logo" src={Logo} />*/}
+					<Link to="/">Dad, Don't Farce!</Link>
+				</Heading>
 				<Button
 					icon={<Search color='brand'/>}
 					onClick={() => setSearch(search => !search)}
---
+++ dadjokes/src/Data/Data.js
@@ -0,0 +1,50 @@
+export default [
+	{
+		id: 0,
+		joke: "What is a witch's favorite subject in school?",
+		punch: "Spelling!",
+		bgColor: "#8B008B"
+	},
+	{
+		id: 2,
+		joke: "What did one wall say to the other wall?",
+		punch: "I'll meet you at the corner!",
+		bgColor: "#6B8E23"
+	},
+	{
+		id: 3,
+		joke: "What is a tornado's favorite game to play?",
+		punch: "Twister!",
+		bgColor: "#483D8B"
+	},
+	{
+		id: 4,
+		joke: "How was the snow globe feeling after the storm?",
+		punch: "A little shaken.",
+		bgColor: "#FF7F50"
+	},
+	{
+		id: 5,
+		joke: "Today a man knocked on my door and asked for a small donation towards the local swimming pool.",
+		punch: "I gave him a glass of water.",
+		bgColor: "#CD853F"
+	},
+	{
+		id: 6,
+		joke: "Why can’t you hear a pterodactyl go to the bathroom?",
+		punch: "The p is silent.",
+		bgColor: "#F08080"
+	},
+	{
+		id: 7,
+		joke: " man walked in to a bar with some asphalt on his arm.",
+		punch: 'He said “Two beers please, one for me and one for the road."',
+		bgColor: "#DC143C"
+	},
+	{
+		id: 8,
+		joke: 'Thanks for explaining the word "many" to me.',
+		punch: "It means a lot.",
+		bgColor: "#F5F5DC"
+	}
+];
