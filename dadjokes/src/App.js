7b4d4afc9a904e1dd398c4d5089792059be6af2d
Tgleason1980 <terryscoding@gmail.com>
2020-01-06 22:48:50 -0600

    commented out Dashboard

--- dadjokes/src/App.js
+++ dadjokes/src/App.js
@@ -2,7 +2,7 @@ import './css/App.css';
 
 import React, { useState } from 'react';
 import { Route } from "react-router-dom";
-import Dashboard from "./Components/Dashboard";
+// import Dashboard from "./Components/Dashboard";
 import { Grommet } from 'grommet';
 
 import Branding from "./Components/Branding/Branding";
@@ -40,7 +40,7 @@ function App() {
           return <EntryLogin {...routeProps} />
         }}
       />
-      <Route exact path='/dashboard' component={Dashboard}/>
+      {/* <Route exact path='/dashboard' component={Dashboard}/> */}
       {/* <Route path='/dashboard' 
       render= {routeProps =>{
       return <Dashboard {...routeProps}/>
