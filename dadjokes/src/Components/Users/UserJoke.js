645246f85d870e5b5207c984176964c144f09415
Michael Padiernos <mike.padiernos@gmail.com>
2020-01-09 04:40:17 -0500

    ADD: - worked on presentation for dashboard, user jokes list, & user jokes

--- dadjokes/src/Components/Users/UserJoke.js
+++ dadjokes/src/Components/Users/UserJoke.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Box, Button } from 'grommet';
+import {FormEdit, FormTrash } from "grommet-icons";
 
 import JokeText from "../Joke/JokeText";
 
@@ -10,14 +11,18 @@ const UserJoke = (props) => (
 			<Box tag="div" width="100%">
 				{props.text}
 			</Box>
-			<Box tag="div" margin={{ top: "1rem" }} width="100%">
+			<Box tag="div" margin={{ top: "0.5rem" }} width="100%">
 				{props.punch}
 			</Box>
-			<Box>
-				<Button />
-			</Box>
-			<Box>
-				<Button />
+			<Box tag="div" flex direction="row" margin={{ top: "0.5rem" }}>
+				<Button icon={<FormEdit color='brand'/>}  className="button button-edit"/>
+				<Button icon={<FormTrash color='brand'/>} className="button button-del" />
+				{/*<Box pad="0">*/}
+
+				{/*</Box>*/}
+				{/*<Box pad="0">*/}
+
+				{/*</Box>*/}
 			</Box>
 		</JokeText>
 	</article>
